5fd57e77e2a47a2941347bbbed372f4e6486c927
Kevin Mulvey <kmulvey@linux.com>
2026-07-12 10:51:52 -0600

    remove unused functions

--- human_numbers.go
+++ human_numbers.go
@@ -3,7 +3,6 @@ package humannumbers
 import (
 	"errors"
 	"fmt"
-	"strconv"
 	"strings"
 )
 
@@ -31,95 +30,6 @@ func Parse(humanString string) (int, error) {
 	}
 
 	return parseIntString(humanString), nil
-}
-
-// convertHumanStringToNumberSlice loops through the give string and places the
-// numeric equivalent to each word in an array of ints
-// e.g. input: "two hundred and forty seven thousand six hundred and twenty four
-//
-//	output: []int{2, 100, 40, 7, 1000, 6, 100, 20, 4}
-func convertHumanStringToNumberSlice(humanString string) ([]int, error) {
-	var humanArr = strings.Fields(humanString)
-	var numbers = make([]int, len(humanArr))
-
-	for i, word := range humanArr {
-		if num, has := baseNumbers[word]; has {
-			numbers[i] = num
-		} else if num, has := decades[word]; has {
-			numbers[i] = num
-		} else if word == "hundred" {
-			numbers[i] = 100
-		} else if num, has := largeMagnitudes[word]; has {
-			numbers[i] = num
-		} else {
-			return nil, fmt.Errorf("%w: '%s'", errUnknownWord, word)
-		}
-	}
-	return numbers, nil
-}
-
-// compressNumberSliceToInt takes an int slice of numbers
-// and either adds or multiplies them depending on their
-// placement in the slice until there is only one
-// element in the slice.
-// e.g. input: []int{2, 100, 40, 7, 1000, 6, 100, 20, 4}
-//
-//	output: 247624
-func compressNumberSliceToInt(numbers []int) (float64, error) {
-
-	if len(numbers) == 1 {
-		return float64(numbers[0]), nil
-	}
-
-	// calculate decades
-	for i := 0; i < len(numbers)-1; i++ {
-		if numbers[i] >= 20 && numbers[i] <= 90 {
-			if numbers[i+1] > 0 && numbers[i+1] < 10 {
-				numbers[i] += numbers[i+1]
-				numbers = remove(numbers, i+1)
-			}
-		}
-	}
-
-	// calculate hundreds
-	for i, num := range numbers {
-		if num == 100 && i > 0 {
-			if numbers[i-1] > 0 && numbers[i-1] < 10 {
-				numbers[i] = num * numbers[i-1]
-				numbers = remove(numbers, i-1)
-				i--
-			}
-		}
-		if i < len(numbers)-1 && numbers[i] >= 100 && numbers[i] < 1000 {
-			if numbers[i+1] > 0 && numbers[i+1] < 100 {
-				numbers[i] += numbers[i+1]
-				numbers = remove(numbers, i+1)
-			}
-		}
-	}
-
-	// calculate large multiples i.e. the numbers before the large one
-	for i := 0; i < len(numbers); i++ {
-		if i > 0 && numbers[i] >= 1000 && numbers[i-1] < 1000 {
-			numbers[i] *= numbers[i-1]
-			numbers = remove(numbers, i-1)
-			i--
-		}
-	}
-
-	// add on larger number specificity i.e. the numbers after the large one
-	for i := len(numbers) - 1; i > 0; i-- {
-		if numbers[i] < numbers[i-1] {
-			numbers[i-1] += numbers[i]
-			numbers = remove(numbers, i)
-		}
-	}
-
-	if len(numbers) != 1 {
-		return 0.0, fmt.Errorf("%w: %+v", errNumberArrayNotReduced, numbers)
-	}
-
-	return float64(numbers[0]), nil
 }
 
 func parseIntString(humanString string) int {
@@ -171,55 +81,6 @@ func validateInput(humanString string) error {
 	}
 
 	return nil
-}
-
-// floatToString is a work in progress, its intention is to turn floats into human text.
-func floatToString(number float64) string {
-	var numArr = strings.Split(strconv.FormatFloat(number, 'f', -1, 64), ".")
-	var minorMultiple = 1
-	var majorMultiple = 1
-	var wordsArr []string
-
-	// take care of the decimals
-	if len(numArr) == 2 {
-		var decimalArr = numArr[1]
-		for i := len(decimalArr) - 1; i >= 0; i-- {
-			wordsArr = append([]string{baseReverse[int(decimalArr[i]-'0')]}, wordsArr...)
-		}
-		wordsArr = append([]string{"dot"}, wordsArr...)
-	}
-
-	var nextIsMajor bool
-	var wholeNumber = numArr[0]
-	for i := len(wholeNumber) - 1; i >= 0; i-- {
-
-		if nextIsMajor {
-			var largeMag = largeMagToString(int(wholeNumber[i]-'0') * majorMultiple)
-			if largeMag != "" {
-				wordsArr = append([]string{largeMag}, wordsArr...)
-			}
-			nextIsMajor = false
-		}
-
-		switch minorMultiple {
-		case 1:
-			wordsArr = append([]string{baseReverse[int(wholeNumber[i]-'0')]}, wordsArr...)
-		case 10:
-			decade, has := decadesReverse[int(wholeNumber[i]-'0')*minorMultiple]
-			if has {
-				wordsArr = append([]string{decade}, wordsArr...)
-			}
-		case 100:
-			wordsArr = append([]string{baseReverse[int(wholeNumber[i]-'0')], "hundred"}, wordsArr...)
-			nextIsMajor = true
-		}
-		minorMultiple *= 10
-		majorMultiple *= 10
-		if minorMultiple == 1000 {
-			minorMultiple = 1
-		}
-	}
-	return strings.Join(wordsArr, " ")
 }
 
 func remove(slice []int, s int) []int {
